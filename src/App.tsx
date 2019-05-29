f5babd6b47f8c4752529b739bfd52eaed083c765
Jordan Place <tapman90@gmail.com>
2019-05-29 11:00:56 -0700

    wow it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Transposit } from "transposit";
 
-const App: React.FC = () => {
+const transposit = new Transposit(
+  "https://dopeee-52ajj.staging-transposit.com"
+);
+
+const Index: React.FC = () => {
+  const [result, setResult] = useState(null);
+
+  if (!transposit.isLoggedIn()) {
+    window.location.href = "/signin";
+    return null;
+  }
+
+  transposit
+    .runOperation("untitled", {})
+    .then(response => {
+      if (response.status !== "SUCCESS") {
+        throw response;
+      }
+      setResult(response.result.results![0]);
+    })
+    .catch(response => {
+      console.log(response);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => transposit.logOut(`${window.location.origin}/signin`)}
         >
-          Learn React
-        </a>
+          Sign out
+        </button>
+        {result && <h1>{result}</h1>}
       </header>
     </div>
   );
-}
+};
+
+const Signin: React.FC = () => {
+  const signInUri: string = transposit.startLoginUri(
+    `${window.location.origin}/handle-signin`
+  );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href={signInUri}>Sign in</a>
+      </header>
+    </div>
+  );
+};
+
+const HandleSignin: React.FC = () => {
+  try {
+    transposit.handleLogin(() => {
+      window.location.href = "/";
+    });
+  } catch (err) {
+    console.log(err);
+    window.location.href = "/signin";
+  }
+
+  return null;
+};
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Index} />
+      <Route path="/signin" exact component={Signin} />
+      <Route path="/handle-signin" exact component={HandleSignin} />
+    </Router>
+  );
+};
 
 export default App;
